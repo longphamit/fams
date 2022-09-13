0c149a8f3be31b4e6241031d11366cb9e3a56138
longphamit <longphamjustdoit@gmail.com>
2022-09-13 09:57:24 +0700

    update

---
+++ rest-gateway/src/main/java/com/fams/restgateway/RestGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fams.restgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestGatewayApplication.class, args);
+    }
+
+}
---
+++ rest-gateway/src/test/java/com/fams/restgateway/RestGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fams.restgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
